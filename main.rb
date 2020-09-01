dae3e1de6f4867a635dbb7cf213bbcc4502a5c1b
SunnySparks <josediazt6@outlook.com>
2020-09-01 12:39:16 -0500

    Correct rubocop offenses

--- main.rb
+++ main.rb
@@ -1,50 +1,47 @@
+# frozen_string_literal: true
+
 puts "
 
-_______ _          _______             _______         
-|__   __(_)        |__   __|           |__   __|        
-   | |   _  ___ ______| | __ _  ___ ______| | ___   ___ 
+_______ _          _______             _______
+|__   __(_)        |__   __|           |__   __|
+   | |   _  ___ ______| | __ _  ___ ______| | ___   ___
    | |  | |/ __|______| |/ _` |/ __|______| |/ _ ) / _ )
    | |  | | (__       | | (_| | (__       | | (_) |  __/
    |_|  |_|(___|      |_|(__,_|(___|      |_|(___/ (___|
 
 "
 
-puts "Insert name for Player X"
+puts 'Insert name for Player X'
 player_x = gets.chomp
-puts "Insert name for Player O"
+puts 'Insert name for Player O'
 player_o = gets.chomp
 
-def display_board (ttt_board)
-
-    puts "#{ttt_board[0]}(0) | #{ttt_board[1]}(1)   |#{ttt_board[2]}(2)"
-    puts "______________________"
-    puts "#{ttt_board[3]}(3) | #{ttt_board[4]}(4)   |#{ttt_board[5]}(5)"
-    puts "______________________"
-    puts "#{ttt_board[6]}(6) | #{ttt_board[7]}(7)   |#{ttt_board[8]}(8)"
-
+def display_board(ttt_board)
+  puts "#{ttt_board[0]}(0) | #{ttt_board[1]}(1)   |#{ttt_board[2]}(2)"
+  puts '______________________'
+  puts "#{ttt_board[3]}(3) | #{ttt_board[4]}(4)   |#{ttt_board[5]}(5)"
+  puts '______________________'
+  puts "#{ttt_board[6]}(6) | #{ttt_board[7]}(7)   |#{ttt_board[8]}(8)"
 end
 
-
-
- board = [" ", " ", " ", " ", " ", " "," ", " ", " "]
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 display_board(board)
 
 puts "#{player_x} pick a number 0-8"
-board = [" "," "," "," "," "," "," "," "," "]
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 x_picked = gets.chomp
-token_x ="X"
-board[x_picked.to_i] = token_x 
+token_x = 'X'
+board[x_picked.to_i] = token_x
 puts "you chose #{x_picked.to_i}"
-#display_board(board)
-
+# display_board(board)
 
 puts "#{player_o} pick a number 0-8"
-board = [" "," "," "," "," "," "," "," "," "]
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 o_picked = gets.chomp
-token_o ="O"
-board[o_picked.to_i] = token_o 
+token_o = 'O'
+board[o_picked.to_i] = token_o
 puts "you chose #{o_picked.to_i}"
-#display_board(board)
+# display_board(board)
