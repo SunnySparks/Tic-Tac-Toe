00e5a108331d59fd157b694c6d8e9c94aa668592
SunnySparks <josediazt6@outlook.com>
2020-09-01 15:33:24 -0500

    Update game UI

--- main.rb
+++ main.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
-puts "
+puts '
 
 _______ _          _______             _______
 |__   __(_)        |__   __|           |__   __|
    | |   _  ___ ______| | __ _  ___ ______| | ___   ___
-   | |  | |/ __|______| |/ _` |/ __|______| |/ _ ) / _ )
+   | |  | |/ __|______| |/ _` |/ __|______| |/ _ \ / _ \
    | |  | | (__       | | (_| | (__       | | (_) |  __/
-   |_|  |_|(___|      |_|(__,_|(___|      |_|(___/ (___|
+   |_|  |_|\___|      |_|\__,_|\___|      |_|\___/ \___|
 
-"
-
-puts 'Insert name for Player X'
+'
+#In this section, we will prompt the users' names to be identified
+puts "Insert name for Player X"
 player_x = gets.chomp
-puts 'Insert name for Player O'
+puts "Alrigth #{player_x}, you will be the Xs"
+puts
+puts "Insert name for Player O"
 player_o = gets.chomp
+puts "And #{player_o}, you will be the Os"
+puts
 
 def display_board(ttt_board)
   puts "#{ttt_board[0]}(0) | #{ttt_board[1]}(1)   |#{ttt_board[2]}(2)"
@@ -26,22 +30,31 @@ end
 
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-display_board(board)
 
+#Player 1 will pick a number
 puts "#{player_x} pick a number 0-8"
+#The board is displayed so the user can see which number he will pick
+display_board(board)
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
+#Here the program shows the selection of player 1
 x_picked = gets.chomp
 token_x = 'X'
 board[x_picked.to_i] = token_x
-puts "you chose #{x_picked.to_i}"
-# display_board(board)
+puts
+puts "#{player_x} chose #{x_picked.to_i}"
+display_board(board)
+puts
 
+#It is player 2's turn to pick a number
 puts "#{player_o} pick a number 0-8"
+display_board(board)
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
+#Here is displayed the selection of player 2
 o_picked = gets.chomp
 token_o = 'O'
 board[o_picked.to_i] = token_o
-puts "you chose #{o_picked.to_i}"
-# display_board(board)
+puts
+puts "#{player_o}  chose #{o_picked.to_i}"
+display_board(board)
