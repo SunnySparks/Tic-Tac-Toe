3a9f26510fb8c370bc3946dd8ed5c1df74dd84a6
SunnySparks <josediazt6@outlook.com>
2020-09-03 12:04:35 -0500

    Correct rubocop offenses

--- main.rb
+++ main.rb
@@ -10,12 +10,12 @@ _______ _          _______             _______
    |_|  |_|\___|      |_|\__,_|\___|      |_|\___/ \___|
 
 '
-#In this section, we will prompt the users' names to be identified
-puts "Insert name for Player X"
+# In this section, we will prompt the users' names to be identified
+puts 'Insert name for Player X'
 player_x = gets.chomp
 puts "Alrigth #{player_x}, you will be the Xs"
 puts
-puts "Insert name for Player O"
+puts 'Insert name for Player O'
 player_o = gets.chomp
 puts "And #{player_o}, you will be the Os"
 puts
@@ -30,14 +30,13 @@ end
 
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-
-#Player 1 will pick a number
+# Player 1 will pick a number
 puts "#{player_x} pick a number 0-8"
-#The board is displayed so the user can see which number he will pick
+# The board is displayed so the user can see which number he will pick
 display_board(board)
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-#Here the program shows the selection of player 1
+# Here the program shows the selection of player 1
 x_picked = gets.chomp
 token_x = 'X'
 board[x_picked.to_i] = token_x
@@ -46,12 +45,12 @@ puts "#{player_x} chose #{x_picked.to_i}"
 display_board(board)
 puts
 
-#It is player 2's turn to pick a number
+# It is player 2's turn to pick a number
 puts "#{player_o} pick a number 0-8"
 display_board(board)
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-#Here is displayed the selection of player 2
+# Here is displayed the selection of player 2
 o_picked = gets.chomp
 token_o = 'O'
 board[o_picked.to_i] = token_o
