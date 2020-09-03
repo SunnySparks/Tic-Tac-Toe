45c3d5e14dc6047ac52279977c1bae8bb03f1eb1
atenaiis <attennaisc@gmail.com>
2020-09-03 13:00:38 -0500

    worked on rubocop offenses

--- main.rb
+++ main.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 puts '
-
 _______ _          _______             _______
 |__   __(_)        |__   __|           |__   __|
    | |   _  ___ ______| | __ _  ___ ______| | ___   ___
    | |  | |/ __|______| |/ _` |/ __|______| |/ _ \ / _ \
    | |  | | (__       | | (_| | (__       | | (_) |  __/
    |_|  |_|\___|      |_|\__,_|\___|      |_|\___/ \___|
-
 '
 # In this section, we will prompt the users' names to be identified
 puts 'Insert name for Player X'
