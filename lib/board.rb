7465ef6a521e153a9e87e84b0e85e15a92aa8c4c
SunnySparks <josediazt6@outlook.com>
2020-09-24 15:47:32 -0500

    fix rubocop offenses

--- lib/board.rb
+++ lib/board.rb
@@ -1,47 +1,43 @@
 class Board
-    attr_accessor :board
+  attr_accessor :board
 
-    def initialize
-       reset!
-      
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @board = Array.new ['', '', '', '', '', '', '', '', '']
+  end
+
+  def display_board
+    puts "#{@board[0]}(1) | #{@board[1]} (2)  |#{@board[2]}(3)"
+    puts '______________________'
+    puts "#{@board[3]}(4) | #{@board[4]} (5)  |#{@board[5]}(6)"
+    puts '______________________'
+    puts "#{@board[6]}(7) | #{@board[7]} (8)  |#{@board[8]}(9)"
+  end
+
+  def next_player(current_player, turn)
+    if current_player == turn[1]
+      turn[0]
+    elsif current_player == turn[0]
+      turn[1]
     end
+  end
 
-    def reset!
-      @board = Array.new ([" ", " ", " ", " ", " ", " ", " ", " ", " "])
-    end 
-
-    def display_board
-        puts "#{@board[0]}(1) | #{@board[1]} (2)  |#{@board[2]}(3)"
-        puts '______________________'
-        puts "#{@board[3]}(4) | #{@board[4]} (5)  |#{@board[5]}(6)"
-        puts '______________________'
-        puts "#{@board[6]}(7) | #{@board[7]} (8)  |#{@board[8]}(9)"
-      end
-
-      
-     
-      def next_player(current_player, turn)
-        if current_player == turn[1]
-         turn[0]
-        elsif current_player ==turn[0]
-         turn[1]
-        end
-      end
-  
-    def taken? (input)
-    if  @board [input]== "X" || @board[input]== "O"
+  def taken?(input)
+    if @board [input] == 'X' || @board[input] == 'O'
       false
-      else
-        true
-      end 
+    else
+      true
     end
+  end
 
-    def valid_move? (input)
-      input.to_i.between?(1,9) && !taken?(input)
-      end
+  def valid_move?(input)
+    input.to_i.between?(1, 9) && !taken?(input)
+  end
 
-      def update (input, player)
-        board[input] = player.token
+  def update(input, player)
+    board[input] = player.token
   end
 end
- 
--- lib/player1.rb
+++ lib/player1.rb
@@ -1,8 +1,8 @@
 class Player
-    attr_accessor :name
-    attr_reader :token
-    def initialize(name, token)
-      @name = name
-      @token = token
-    end
+  attr_accessor :name
+  attr_reader :token
+  def initialize(name, token)
+    @name = name
+    @token = token
   end
+end
