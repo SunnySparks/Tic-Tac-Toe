5f28e1d43a2f751e85d2d8bb5f1825c37486eddb
SunnySparks <josediazt6@outlook.com>
2020-09-25 11:13:25 -0500

    remove puts from board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -10,11 +10,11 @@ class Board
   end
 
   def display_board
-    puts "#{@board[0]}(1) | #{@board[1]} (2)  |#{@board[2]}(3)"
-    puts '______________________'
-    puts "#{@board[3]}(4) | #{@board[4]} (5)  |#{@board[5]}(6)"
-    puts '______________________'
-    puts "#{@board[6]}(7) | #{@board[7]} (8)  |#{@board[8]}(9)"
+    "    #{@board[0]}(1) | #{@board[1]} (2)  |#{@board[2]}(3)
+  ______________________
+    #{@board[3]}(4) | #{@board[4]} (5)  |#{@board[5]}(6)
+  ______________________
+    #{@board[6]}(7) | #{@board[7]} (8)  |#{@board[8]}(9)"
   end
 
   def next_player(current_player, turn)
