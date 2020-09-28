476b45de26503936852f8fcbb8dfe055748766db
atenaiis <attennaisc@gmail.com>
2020-09-28 10:31:32 -0500

    remove DRY for win

--- lib/board.rb
+++ lib/board.rb
@@ -16,6 +16,18 @@ class Board
   ______________________
     #{@board[6]}(7) | #{@board[7]} (8)  |#{@board[8]}(9)"
   end
+  def win
+    @win = false
+    @win = true if @board[0] == @board[1] && @board[1] == @board[2]
+    @win = true if @board[0] == @board[4] && @board[4] == @board[8]
+    @win = true if @board[0] == @board[3] && @board[3] == @board[6]
+    @win = true if @board[1] == @board[4] && @board[4] == @board[7]
+    @win = true if @board[2] == @board[4] && @board[4] == @board[6]
+    @win = true if @board[2] == @board[5] && @board[5] == @board[8]
+    @win = true if @board[3] == @board[4] && @board[4] == @board[5]
+    @win = true if @board[6] == @board[7] && @board[7] == @board[8]
+    @win
+  end
 
   def next_player(current_player, turn)
     if current_player == turn[1]
